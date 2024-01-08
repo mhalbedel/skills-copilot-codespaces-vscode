c29b8b520777f658d192db8ba7dbf53f2a8c4325
Markus Halbedel <markus@halbedel.com>
2024-01-08 10:44:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,5 @@
+function skillsmember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var skillsmember = document.getElementById("skillsmemb");
+}
